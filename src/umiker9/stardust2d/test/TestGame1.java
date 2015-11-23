f3eaa72e83d646c3806701b6580e25b8b770bf33
miker9 <umiker9@gmail.com>
2015-11-23 15:03:48 +0400

    Added texture to sprite

--- src/umiker9/stardust2d/test/TestGame1.java
+++ src/umiker9/stardust2d/test/TestGame1.java
@@ -2,9 +2,11 @@ package umiker9.stardust2d.test;
 
 import umiker9.stardust2d.BasicGame;
 import umiker9.stardust2d.Sprite;
-import umiker9.stardust2d.graphics.lwjgl2.Renderer;
 import umiker9.stardust2d.Scene;
 import umiker9.stardust2d.Stardust2D;
+import umiker9.stardust2d.graphics.lwjgl2.Texture2D;
+import umiker9.stardust2d.graphics.lwjgl2.TextureLoader;
+import umiker9.stardust2d.systems.io.FileIO;
 
 /**
  * Created by miker9 on 22/11/2015.
@@ -28,8 +30,20 @@ public class TestGame1 extends BasicGame {
     protected void init() {
         super.init();
 
+        Texture2D texture = TextureLoader.loadTexture(FileIO.loadResource("Assets/grid.png"));
+
         mainScene = new Scene();
-        Sprite testSprite = new Sprite(100, 100, 50, 50);
+        Sprite testSprite = new Sprite(texture, 100, 100, 50, 50) {
+            @Override
+            public void update(long delta) {
+                super.update(delta);
+
+                x += delta/(double)Stardust2D.timePrecission*100;
+                rotation += delta/(double)Stardust2D.timePrecission*40;
+            }
+        };
+        testSprite.setOriginX(testSprite.getWidth()/2);
+        testSprite.setOriginY(testSprite.getWidth()/2);
         testSprite.setRotation(10);
         mainScene.add(testSprite);
         setCurrentScene(mainScene);
