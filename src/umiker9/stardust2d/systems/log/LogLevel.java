0cc6ff1dca22da97ec599b91b2c9210620019b4b
slimon0 <slimons.mail@gmail.com>
2015-11-23 12:30:10 +0300

    Renamed packages

--- src/umiker9/stardust2d/systems/log/LogLevel.java
+++ src/umiker9/stardust2d/systems/log/LogLevel.java
@@ -1,1 +1,9 @@
+package umiker9.stardust2d.systems.log;
 
+/**
+ * Created by Notezway on 23.11.2015.
+ */
+public enum LogLevel {
+
+    DEBUG, INFO, WARNING, BULLSHIT
+}
--- src/umiker9/stardust2d/systems/log/Message.java
+++ src/umiker9/stardust2d/systems/log/Message.java
@@ -1,1 +1,27 @@
+package umiker9.stardust2d.systems.log;
 
+/**
+ * Created by Notezway on 23.11.2015.
+ */
+public class Message {
+
+    private LogLevel level;
+    private String text;
+
+    public Message(LogLevel level, String text) {
+        this.level = level;
+        this.text = text;
+    }
+
+    public LogLevel getLevel() {
+        return level;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String toString() {
+        return "[" + level.toString() + "] " + text;
+    }
+}
