b8a6b9b6727071de06cf5b81270ef3c5502df4d7
slimon0 <slimons.mail@gmail.com>
2015-11-23 15:49:04 +0300

    Reduced coolest of Color

--- src/umiker9/stardust2d/graphics/Color.java
+++ src/umiker9/stardust2d/graphics/Color.java
@@ -5,126 +5,81 @@ package umiker9.stardust2d.graphics;
  */
 public class Color {
 
-    private int data;
+    private double r, g, b, a;
 
     public Color(double r, double g, double b) {
-        data = getData(r, g, b, 1);
-    }
-
-    public Color(int r, int g, int b) {
-        set(r, g, b, 255);
+        this(r, g, b, 1);
     }
 
     public Color(double r, double g, double b, double a) {
-        data = getData(r, g, b, a);
+        set(r, g, b, a);
+    }
+
+    public Color(int r, int g, int b) {
+        this(r, g, b, 255);
     }
 
     public Color(int r, int g, int b, int a) {
         set(r, g, b, a);
     }
 
-    public Color(int color) {
-        data = color;
+    public Color set(double r, double g, double b, double a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+        return this;
     }
 
-    public void set(int color) {
-        data = color;
+    public Color set(int r, int g, int b, int a) {
+        this.r = r/255.;
+        this.g = g/255.;
+        this.b = b/255.;
+        this.a = a/255.;
+        return this;
     }
 
-    public void set(int r, int g, int b, int a) {
-        data = getData(r, g, b, a);
+    public double getR() {
+        return r;
     }
 
-    public void set(double r, double g, double b, double a) {
-        data = getData(r, g, b, a);
+    public double getG() {
+        return g;
     }
 
-    public int getData() {
-        return data;
+    public double getB() {
+        return b;
     }
 
-    public int[] getComponents() {
-        return getComponents(data);
-    }
-
-    public double[] getDoubleComponents() {
-        return getDoubleComponents(data);
-    }
-
-    public int getR() {
-        return (data & 0xFF000000) >> 24;
-    }
-
-    public int getG() {
-        return (data & 0x00FF0000) >> 16;
-    }
-
-    public int getB() {
-        return (data & 0x0000FF00) >> 8;
-    }
-
-    public int getA() {
-        return (data & 0x000000FF);
-    }
-
-    public double getDR() {
-        return ((data & 0xFF000000) >> 24) /255.;
-    }
-
-    public double getDG() {
-        return ((data & 0x00FF0000) >> 16) /255.;
-    }
-
-    public double getDB() {
-        return ((data & 0x0000FF00) >> 8) /255.;
-    }
-
-    public double getDA() {
-        return ((data & 0x000000FF)) /255.;
+    public double getA() {
+        return a;
     }
 
     public Color setR(int r) {
-        data = (data & 0x00FFFFFF) | (r << 24);
+        this.r = r;
         return this;
     }
 
     public Color setG(int g) {
-        data = (data & 0xFF00FFFF) | (g << 16);
+        this.g = g;
         return this;
     }
 
     public Color setB(int b) {
-        data = (data & 0xFFFF00FF) | (b << 8);
+        this.b = b;
         return this;
     }
 
     public Color setA(int a) {
-        data = (data & 0xFFFFFF00) | a;
-        return this;
-    }
-
-    public Color setR(double r) {
-        data = (data & 0x00FFFFFF) | ((int)r*255 << 24);
-        return this;
-    }
-
-    public Color setG(double g) {
-        data = (data & 0xFF00FFFF) | ((int)g*255 << 16);
-        return this;
-    }
-
-    public Color setB(double b) {
-        data = (data & 0xFFFF00FF) | ((int)b*255 << 8);
-        return this;
-    }
-
-    public Color setA(double a) {
-        data = (data & 0xFFFFFF00) | ((int)a*255);
+        this.a = a;
         return this;
     }
 
     public Color mix(Color c) {
-        data = mixColors(data, c.getData());
+        this.r = r + c.r;
+        this.g = g + c.g;
+        this.b = b + c.b;
+        this.a = a + c.a;
         return this;
     }
 
@@ -136,7 +91,7 @@ public class Color {
         return (int)(r * 255) << 24 | (int)(g * 255) << 16 | (int)(b * 255) << 8 | (int)(a * 255);
     }
 
-    public static int[] getComponents(int color) {
+    public static int[] getIntComponents(int color) {
         return new int[] {
                 (color & 0xFF000000) >> 24,
                 (color & 0x00FF0000) >> 16,
@@ -145,7 +100,7 @@ public class Color {
         };
     }
 
-    public static double[] getDoubleComponents(int color) {
+    public static double[] getComponents(int color) {
         return new double[] {
                 ((color & 0xFF000000) >> 24) /255.,
                 ((color & 0x00FF0000) >> 16) /255.,
@@ -154,20 +109,9 @@ public class Color {
         };
     }
 
-    public static int mixColors(int color1, int color2) {
-        return  (((color1 & 0xFF000000) >> 24) + ((color2 & 0xFF000000) >> 24)) |
-                (((color1 & 0x00FF0000) >> 16) + ((color2 & 0x00FF0000) >> 16)) |
-                (((color1 & 0x0000FF00) >> 8)  + ((color2 & 0x0000FF00) >> 8))  |
-                (((color1 & 0x000000FF))       + ((color2 & 0x000000FF)));
-    }
-
-    public static Color mixColors(Color color1, Color color2) {
-        return color1.mix(color2);
-    }
-
-    public static final Color WHITE = new Color(255, 255, 255);
-    public static final Color BLACK = new Color(0, 0, 0);
-    public static final Color RED = new Color(255, 0, 0);
-    public static final Color GREEN = new Color(0, 255, 0);
-    public static final Color BLUE = new Color(0, 0, 255);
+    public static final Color WHITE = new Color(1., 1., 1.);
+    public static final Color BLACK = new Color(0., 0., 0.);
+    public static final Color RED = new Color(1., 0., 0.);
+    public static final Color GREEN = new Color(0., 1., 0.);
+    public static final Color BLUE = new Color(0., 0., 1.);
 }
